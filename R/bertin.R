541953254969c51f216f1f484af194d567f0a314
nfrerebeau <nicolas.frerebeau@u-bordeaux-montaigne.fr>
2025-04-28 13:58:06 +0200

    Add a module for Ford and Bertin diagrams

---
+++ R/bertin.R
@@ -0,0 +1,109 @@
+# UI ===========================================================================
+#' Bertin Diagrams UI
+#'
+#' @param id A [`character`] vector to be used for the namespace.
+#' @param title A [`character`] string giving the plot title.
+#' @seealso [bertin_server()]
+#' @family count data modules
+#' @keywords internal
+#' @export
+bertin_ui <- function(id, title = NULL) {
+  # Create a namespace function using the provided id
+  ns <- NS(id)
+
+  nav_panel(
+    title = tr_("Plot"),
+    layout_sidebar(
+      sidebar = sidebar(
+        ## Input: select plot
+        radioButtons(
+          inputId = ns("type"),
+          label = tr_("Type of plot"),
+          selected = "ford",
+          choiceNames = c(tr_("Ford diagram"), tr_("Bertin barplot"),
+                          tr_("Bertin scalogram"), tr_("Heatmap")),
+          choiceValues = c("ford", "barplot", "scalogram", "heatmap")
+        ),
+        conditionalPanel(
+          condition = "input.type == 'ford'",
+          ns = ns,
+          checkboxInput(
+            inputId = ns("eppm"),
+            label = "EPPM",
+            value = FALSE
+          ),
+          checkboxInput(
+            inputId = ns("weights"),
+            label = tr_("Weights"),
+            value = FALSE
+          )
+        ),
+        conditionalPanel(
+          condition = "input.type == 'barplot'",
+          ns = ns,
+          radioButtons(
+            inputId = ns("threshold"),
+            label = tr_("Threshold"),
+            selected = "none",
+            choiceNames = c(tr_("None"), tr_("Mean"), tr_("Median")),
+            choiceValues = c("none", "mean", "median")
+          )
+        ),
+        conditionalPanel(
+          condition = "input.type == 'heatmap'",
+          ns = ns,
+          select_color(id = ns("col"), type = "sequential")
+        )
+      ),
+      output_plot(
+        id = ns("plot"),
+        height = "100%",
+        title = title
+      )
+    )
+  )
+}
+
+# Server =======================================================================
+#' Bertin Diagrams Server
+#'
+#' @param id An ID string that corresponds with the ID used to call the module's
+#'  UI function.
+#' @param x A reactive `data.frame` (typically returned by [import_server()]).
+#' @seealso [bertin_ui()]
+#' @family count data modules
+#' @keywords internal
+#' @export
+bertin_server  <- function(id, x) {
+  stopifnot(is.reactive(x))
+
+  moduleServer(id, function(input, output, session) {
+    ## Plot -----
+    plot_permute <- reactive({
+      req(x())
+
+      threshold <- switch(
+        input$threshold,
+        mean = mean,
+        median = median,
+        none = NULL
+      )
+
+      color <- get_color("col")()
+      switch(
+        input$type,
+        ford = function()
+          tabula::plot_ford(x(), weights = input$weights, EPPM = input$eppm),
+        barplot = function()
+          tabula::plot_bertin(x(), threshold = threshold),
+        scalogram = function()
+          tabula::plot_spot(x(), color = "black", legend = FALSE),
+        heatmap = function()
+          tabula::plot_heatmap(x(), color = color, fixed_ratio = FALSE)
+      )
+    })
+
+    ## Render plot -----
+    render_plot("plot", x = plot_permute)
+  })
+}
